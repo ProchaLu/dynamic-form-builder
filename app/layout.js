2163156b5e4503ae0428e9698ee5a6a0ca3bc270
Lukas Prochazka <prochazka.lu@gmail.com>
2025-06-03 09:33:47 +0200

    Initialize project

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import './globals.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const dynamic = 'force-dynamic';
+
+export const metadata = {
+  title: 'Dynamic Form Builder',
+  description:
+    'Create custom forms by adding, removing, and reordering fields. Supports text, number, date, and dropdown fields',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-6">Dynamic Form Builder</h1>
+      <p className="text-gray-900 mb-8">
+        Create custom forms by adding, removing, and reordering fields. Supports
+        text, number, date, and dropdown fields.
+      </p>
+    </div>
+  );
+}
